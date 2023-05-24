498eb75ed6ed494f1712383232ceb7dc58292092
Kennedy Musau <kennedy.musauu@gmail.com>
2023-05-24 18:18:29 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [image, setImage]= useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((responese) => responese.json())
+      .then((data) => setImage(data.message))
+      .catch(error => console.error(error));
+    setIsLoading(true);
+  },[]);
+
+  if(!isLoading) return <h2>Loading ...</h2>
+  return (
+    <div>
+      <img src={image} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
